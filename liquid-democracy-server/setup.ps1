56af0ee8ff1f540b6d75c4df70fd03ab569b967e
Vict2976 <vibr@itu.com>
2023-02-15 10:42:07 +0100

    final changes to script

--- liquid-democracy-server/setup.ps1
+++ liquid-democracy-server/setup.ps1
@@ -1,4 +1,5 @@
 $password = New-Guid
+docker ps -aq | xargs docker stop
 if(echo uname -m == "x86_64") {
     docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
 } else {
@@ -11,6 +12,7 @@ dotnet user-secrets init
 dotnet user-secrets set "ConnectionStrings:liquiddemocracy" "$connectionString"
 Set-Clipboard -Value  $connectionString
 cd ../Entities/
+rm -r ./Migrations/
 dotnet ef migrations add InitialMigration -s ../API/
 dotnet ef database update -s ../API/
 cd ..
