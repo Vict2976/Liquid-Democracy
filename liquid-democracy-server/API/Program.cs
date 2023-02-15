66913289f08728852e7a7c83f38f3e56bdeec107
Vict2976 <vibr@itu.com>
2023-02-15 10:26:59 +0100

    Changes setup script

--- liquid-democracy-server/API/Program.cs
+++ liquid-democracy-server/API/Program.cs
@@ -4,24 +4,24 @@ using Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<LiquidDemocracyContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("liquiddemocracy")));
 builder.Services.AddScoped<ILiquidDemocracyContext, LiquidDemocracyContext>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+/* using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<LiquidDemocracyContext>();
     db.Database.Migrate();
-}
+} */
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- liquid-democracy-server/setup.ps1
+++ liquid-democracy-server/setup.ps1
@@ -1,5 +1,9 @@
 $password = New-Guid
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+if(echo uname -m == "x86_64") {
+    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+} else {
+    docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e "SA_PASSWORD=$password" -p 1433:1433 -d mcr.microsoft.com/azure-sql-edge
+}
 $database = "liquiddemocracy"
 $connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$password"
 cd ./API/
