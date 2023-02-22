cc7d09ebd1ad14af716f67eb508db6c55eaa0841
niwl <niwl@itu.dk>
2023-02-22 14:53:59 +0100

    displays singleElection very ugly

--- liquid-democracy-web/src/pages/Election.tsx
+++ liquid-democracy-web/src/pages/Election.tsx
@@ -1,19 +1,36 @@
-import {Link, useLocation} from "react-router-dom";
-
-interface IElection {
-    ElectionId: number;
-}
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { GetElectionFromId } from "../services/election.service";
+import { Election } from "../builder/Interface";
 
 //Får Id med videre og så kalde en fetch på /GetElectionByID
 
+export default function ElectionFunc() {
+  const [election, setElection] = useState<Election>();
 
-export default function Election(){
-    let { state } = useLocation();
-    return (
-        <view>
-            <button onClick={()=> console.log(state)}>
-                Tryk mig
-            </button>
-        </view>
-      );
+  let { state } = useLocation();
+  let id = Number(state);
+  /*     const singleElection = () => {
+        GetElectionFromId(Number(state)).then((elect) => {
+            setElection(elect)
+          });
+    } */
+
+  useEffect(() => {
+    GetElectionFromId(id).then((messages) => {
+      setElection(messages);
+    });
+  }, []);
+
+  return (
+    <view>
+      <ul>
+        <li>{election?.name}</li>
+        <li>{election?.electionId}</li>
+        <li>{election?.amountOfVotes}</li>
+        <li>{election?.candidates}</li>
+        <li>{election?.votings}</li>
+      </ul>
+    </view>
+  );
 }
