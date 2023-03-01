82e2f617639371c150701983cb2228e0e6045883
niwl <niwl@itu.dk>
2023-03-01 15:54:46 +0100

    Made sure that the same user kan not have two votes in the same election

--- liquid-democracy-server/Repository/IVotingsRepository.cs
+++ liquid-democracy-server/Repository/IVotingsRepository.cs
@@ -4,5 +4,6 @@ public interface IVoteRepository
 {
     public Task<Vote?> CreateAsync(int userId, int elecitonId, int candidateId);
     public Task<IEnumerable<Vote?>> ReadAllAsync();
+    public Task<Vote?> checkForExistingVote(int belongsToId, int elecitonId);
 
 }
