6bc2912eba164257dc82dc302ffc6936355a91bc
Vict2976 <vibr@itu.com>
2023-03-08 16:07:21 +0100

    added mitId

--- liquid-democracy-server/API/Controllers/VoteUsedOnController.cs
+++ liquid-democracy-server/API/Controllers/VoteUsedOnController.cs
@@ -7,30 +7,54 @@ using Repository;
 public class VoteUsedOnController : ControllerBase
 {
     readonly IVoteUsedOnRepository _repository;
+    readonly IVoteRepository _voteRepository;
 
-    public VoteUsedOnController(IVoteUsedOnRepository repository)
+
+    public VoteUsedOnController(IVoteUsedOnRepository repository, IVoteRepository voteRepository)
     {
         _repository = repository;
+        _voteRepository = voteRepository;
     }
 
     [AllowAnonymous]
-    [HttpPost]
-    [Route("/ForCandidate")]
+    [HttpGet]
+    [Route("/ForCandidate/{userId}/{electionId}/{candidateId}")]
     [ProducesResponseType(typeof(VoteUsedOn), 201)]
-    public async Task<ActionResult<VoteUsedOn?>> CreateVoteUsedOnForCandidate([FromBody] VoteUsedOnCandidateDTO voteUsedOnCandidateDTO)
+    public async Task<RedirectResult> CreateVoteUsedOnForCandidate(int userId, int electionId, int candidateId)
     {
-        var response = await _repository.CreateVoteUsedOnForCandidate(voteUsedOnCandidateDTO.voteId, voteUsedOnCandidateDTO.candidateId);
-        return response;
+        var checkForVote = _voteRepository.checkForExistingVote(userId, electionId);
+
+        if (checkForVote.Result != null)
+        {
+            return null;
+        }
+
+        var vote = await _voteRepository.CreateAsync(userId, electionId);
+
+        await _repository.CreateVoteUsedOnForCandidate(vote.VoteId, candidateId);
+
+        return  new RedirectResult(url: "http://localhost:3000/", permanent: true, preserveMethod: true);
     }
 
     [AllowAnonymous]
-    [HttpPost]
-    [Route("/ForDelegate")]
+    [HttpGet]
+    [Route("/ForDelegate/{userId}/{electionId}/{delegateId}")]
     [ProducesResponseType(typeof(VoteUsedOn), 201)]
-    public async Task<ActionResult<VoteUsedOn?>> CreateVoteUsedOnForDelegate([FromBody] VoteUsedOnDelegateDTO voteUsedOnDelegateDTO)
+    public async Task<RedirectResult> CreateVoteUsedOnForDelegate(int userId, int electionId, int delegateId)
     {
-        var response = await _repository.CreateVoteUsedOnForDelegate(voteUsedOnDelegateDTO.voteId, voteUsedOnDelegateDTO.delegateId, voteUsedOnDelegateDTO.electionId);
-        return response;
-    }
+        var checkForVote = _voteRepository.checkForExistingVote(userId, electionId);
 
+        if (checkForVote.Result != null)
+        {
+            return null;
+        }
+
+        var vote = await _voteRepository.CreateAsync(userId, electionId);
+
+
+        var response = await _repository.CreateVoteUsedOnForDelegate(vote.VoteId, delegateId, electionId);
+
+
+        return  new RedirectResult(url: "http://localhost:3000/Register", permanent: true, preserveMethod: true);
+    }
 }
